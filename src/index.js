e5e35f336b61b6ed2683384b8286fa4a07decb7a
getlarge <ed@getlarge.eu>
2019-02-17 00:00:58 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import omaObjects from './oma-object.json';
+import omaResources from './oma-resources.json';
+import omaViews from './oma-views.json';
+
+module.exports = {
+  omaObjects,
+  omaResources,
+  omaViews,
+};

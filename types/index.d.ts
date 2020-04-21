f2c1d86ebf1322c8d9cbf9a2a424b678749f5360
getlarge <ed@getlarge.eu>
2020-04-21 20:08:02 +0200

    update dependencies and add types

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+export * from '../interfaces';
+
+export declare const omaObjects: object[];
+
+export declare const omaResources: object[];
+
+export declare const omaViews: object[];
+
+export declare const version: string;
+
+export declare const openApi: object;

54638d92dd63c20939b86350a1205c3b6e6bc1a2
getlarge <ed@getlarge.eu>
2020-04-23 17:47:28 +0200

    improve typings

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,54 @@
 export * from '../interfaces';
 
-export declare const omaObjects: object[];
+export type Resources = {
+  [key: string]: any;
+};
 
-export declare const omaResources: object[];
+export type OmaObject = {
+  name: string;
+  descripiton: string;
+  value: number;
+  resourceIds: string;
+  resources: Resources;
+};
 
-export declare const omaViews: object[];
+export enum Operations {
+  'R',
+  'W',
+  'R,W',
+  'E',
+}
+
+export enum OmaResourceTypes {
+  'Float',
+  'Integer',
+  'String',
+  'Boolean',
+  'Time',
+  'Opaque',
+  'null',
+}
+
+export type OmaResource = {
+  name: string;
+  descripiton: string;
+  value: number;
+  operations: Operations;
+  type: OmaResourceTypes;
+};
+
+export type OmaView = {
+  name: string;
+  value: number;
+  resources: Resources;
+  icons: string[];
+};
+
+export declare const omaObjects: OmaObject[];
+
+export declare const omaResources: OmaResource[];
+
+export declare const omaViews: OmaView[];
 
 export declare const version: string;
 
